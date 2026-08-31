a3a1eadc5c02cd81aeaec5dddface76f77920f53
Shridaam <shridaam17@gmail.com>
2026-08-31 14:19:24 +0530

    Add initial LeetCode problem data

---
+++ script.js
@@ -0,0 +1,42 @@
+const problems=[
+    {
+        id:1,
+        title:"Two Sum",
+        difficulty:"Easy",
+        topic:"Array",
+        solved:true
+    },{
+        id:2,
+        title:"valid Parantheses",
+        difficulty:"Easy",
+        topic:"Stack",
+        solved:false
+    }, {
+        id: 3,
+        title: "3Sum",
+        difficulty: "Medium",
+        topic: "Array",
+        solved: false
+    },
+    {
+        id: 4,
+        title: "Binary Search",
+        difficulty: "Easy",
+        topic: "Binary Search",
+        solved: true
+    },
+    {
+        id: 5,
+        title: "Maximum Subarray",
+        difficulty: "Medium",
+        topic: "Array",
+        solved: false
+    },
+    {
+        id: 6,
+        title: "Merge k Sorted Lists",
+        difficulty: "Hard",
+        topic: "Linked List",
+        solved: false
+    }
+];
